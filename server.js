7662f78fa13acd22121f2681ca1ea2e998aeac18
jaiminkumar18 <jaiminsonwane@gmail.com>
2025-08-14 19:54:48 +0530

    updated_now

--- server.js
+++ server.js
@@ -37,3 +37,5 @@ app.use(errorHandler);
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+// code update
+
